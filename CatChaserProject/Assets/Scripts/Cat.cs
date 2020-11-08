9864409a677a696bb47df00b519250d0a59b147f
SubtilMatilde <60981647+SubtilMatilde@users.noreply.github.com>
2020-11-08 23:16:32 +0100

    Noice

---
+++ CatChaserProject/Assets/Scripts/Cat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    
+    public float speed = 10;
+    public GameObject fish;
+    public GameHandler gameHandler;
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+
+    private bool clicked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        endPosition = fish.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameHandler.gameIsOn)
+            return;
+        
+        if (!clicked)
+        {
+            if (transform.position != endPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);
+            }
+
+            else
+            {
+                Debug.Log("Noob");
+                gameHandler.Defeat();
+            }
+        }
+        else{
+            transform.position = Vector3.MoveTowards(transform.position, startPosition, (speed + 2) * Time.deltaTime);
+            if (transform.position == startPosition)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        clicked = true;
+        Debug.Log("Miaou");
+    }
+}
---
+++ CatChaserProject/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public bool gameIsOn = true;
+    public GameObject catPrefab;
+    public GameObject fish;
+    public int spawnTime = 2;
+    
+    
+    void Start()
+    {
+        StartCoroutine(SpawnCatsCoroutine(spawnTime));
+    }
+    
+    public void Defeat()
+    {
+        gameIsOn = false;
+    }
+
+    private Vector3 GetRandomVect()
+    {
+        Vector3 stageDimension = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        int delta = Random.Range(2,8);
+        Vector2 topRight = new Vector2(stageDimension.x + delta, stageDimension.y + delta);
+        Vector2 topLeft = new Vector2(-stageDimension.x - delta, stageDimension.y + delta);
+        Vector2 bottomRight = new Vector2(stageDimension.x + delta, -stageDimension.y - delta);
+        Vector2 bottomLeft = new Vector2(-stageDimension.x - delta, -stageDimension.y - delta);
+
+        Vector3 spawnPoint = Vector3.zero;
+        int zoneDecision = Random.Range(0, 4);
+        
+        switch (zoneDecision)
+        {
+            case 0:
+                spawnPoint = new Vector3(topLeft.x, Random.Range(topLeft.y, bottomLeft.y),0);
+                break;
+            case 1:
+                spawnPoint = new Vector3(Random.Range(topLeft.x, topRight.x), topLeft.y,0);
+                break;
+            case 2:
+                spawnPoint = new Vector3(topRight.x, Random.Range(topRight.y, bottomRight.y),0);
+                break;
+            case 3:
+                spawnPoint = new Vector3(Random.Range(bottomLeft.x, bottomRight.x), bottomLeft.y,0);
+                break;
+        }
+        return spawnPoint;
+    }
+
+    private void GenerateCat()
+    {
+        GameObject catGameObject = Instantiate(catPrefab, GetRandomVect(),Quaternion.identity);
+        Cat cat = catGameObject.GetComponent<Cat>();
+        cat.fish = fish;
+        cat.gameHandler = this;
+    }
+
+    private IEnumerator SpawnCatsCoroutine(int waitTime)
+    {
+        while (gameIsOn)
+        {
+            GenerateCat();
+            yield return new WaitForSeconds(waitTime);         
+        }
+    }
+}
